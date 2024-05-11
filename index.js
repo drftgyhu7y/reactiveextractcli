b823f06444ce56147fca6ef5251aad7229b94819
drftgyhu7y <gs1hulala47@hotmail.com>
2024-05-11 10:04:46 +0700

    update

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-const shellSort = (arr) => {
-  const n = arr.length;
-  for (let gap = Math.floor(n / 2); gap > 0; gap = Math.floor(gap / 2)) {
-    for (let i = gap; i < n; i++) {
-      const temp = arr[i];
-      let j;
-      for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
-        arr[j] = arr[j - gap];
-      }
-      arr[j] = temp;
-    }
-  }
-  return arr;
-};
+const minN = (arr, n = 1) => [...arr].sort((a, b) => a - b).slice(0, n);
